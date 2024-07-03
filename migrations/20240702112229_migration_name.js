4cc1f2b11fdbe2e36bb4eba2cd381c310b3289f8
Martin-Andonov <85078216+Martin-Andonov@users.noreply.github.com>
2024-07-03 12:16:48 +0300

    Update database migration
    added constraints on number_to_guess and changed the name to be more detailed

--- migrations/20240702112229_migration_name.js
+++ migrations/20240702112229_migration_name.js
@@ -6,16 +6,16 @@
 export async function up(knex){
     await knex.schema.createTable('games', function(table) {
         table.bigIncrements('id').primary();
-        table.integer('number').notNullable();
-        table.bigInteger('attempts').notNullable(); 
-        table.timestamp('start_time').notNullable();
-        table.timestamp('end_time').notNullable();
+        table.integer('number_to_guess').notNullable().checkBetween([1234, 9876]);
+        table.bigInteger('number_of_attempts').notNullable().defaultTo(0); 
+        table.timestamp('start_time').notNullable().defaultTo(knex.fn.now(6));;
+        table.timestamp('end_time').notNullable().defaultTo(knex.fn.now(6));;
       })
       .createTable('scores', function(table) {
         table.bigIncrements('id').primary();
-        table.string('name').notNullable();
+        table.string('user_name').notNullable();
         table.bigInteger('game_id').unsigned().notNullable();
-        table.foreign('game_id').references('games.id');
+        table.foreign('game_id').references('games.id').onDelete('cascade');
       });
   };
   
