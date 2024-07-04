79463dd5ea87d2ac468d888e2575438fbef96dba
Martin-Andonov <85078216+Martin-Andonov@users.noreply.github.com>
2024-07-04 13:35:27 +0300

    added router and model for guesses

--- index.js
+++ index.js
@@ -4,30 +4,14 @@ import express from 'express';
 import { Model } from 'objection';
 import { Score } from './source/models/score.js';
 import { Game } from './source/models/game.js';
-
+import { Guess } from './source/models/guesses.js';
+import { gamesRouter } from './source/routes/games.js';
 
 const app = express();
 const port = 3000;
 const knexClient = knex(knexConfig.development);  
 Model.knex(knexClient);
-
-
-app.get('/end', (req, res) => {
-  res.send('Bye world!');
-});
-
-
-app.get('/start', async (req, res) => {
-  // await Score.query().insert({
-
-  //   gameId:1,
-  //   userName:'Ivan'
-
-  // });
-  const result = await Game.query().withGraphFetched('score').findById(1)
-  res.send(result);
-
-});
+app.use('/games',gamesRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!!!!`);
