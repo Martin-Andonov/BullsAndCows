dff0de00e4bcc9cb5433fc7af5966d5cdcb4fa67
Martin-Andonov <85078216+Martin-Andonov@users.noreply.github.com>
2024-07-08 15:22:48 +0300

    Added guess/create post method

--- source/routes/guesses.js
+++ source/routes/guesses.js
@@ -37,11 +37,73 @@ guessRouter.get('/by-game/:gameID', async (req, res) => {
     }
 });
 
-guessRouter.post("/create", async(req,res) => 
+
+guessRouter.post("/create/:gameID", async(req,res) => 
 {
+  const guess = String(req.body["guess"]);
+  const gameID = parseInt(req.params.gameID);
+  
+  if (!/^(?!.*(.).*\1)\d{4}$/.test(guess)) 
+  {
+    return res.status(400).json({ status: 'fail', message: 'Error no guess or invalid guess provided!'});
+  }
+  
+  if (!gameID) {
+    return res.status(400).json({ status: 'fail', message: 'Invalid gameId!'});
+  }
 
-})
+  const game = await Game.query().findById(gameID);
 
+  if(!game)
+  {
+    return res.status(400).json({ status: 'fail', message: 'No game with that Id exists!'});
+  } 
+
+  let animals = checkBullsAndCows(game,guess);
+  let hasGuessed = animals.bullsCount === 4;
+
+  const result = await createGuessForGame(gameID,guess,animals.bullsCount,animals.cowsCount);
+
+  if (result) {
+    return res.status(200).json({ status: 'success', bullsCount:animals.bullsCount, cowsCount:animals.cowsCount, hasGuessed:hasGuessed});
+  } else {
+    return res.status(404).json({ status: 'fail', message: 'Error while creating game!' });
+  }
+
+});
+
+function checkBullsAndCows(game,guess)
+{
+  
+  let animalCounts = { 
+    bullsCount: 0,
+    cowsCount: 0
+  }
+  
+  for(let i = 0; i < 4; i++)
+  {
+    if(guess.includes(game.numberToGuess.charAt(i))) 
+    {
+      if(guess.indexOf(game.numberToGuess.charAt(i)) == i)
+      {
+        animalCounts.bullsCount ++;
+      }else{
+        animalCounts.cowsCount ++;
+      }
+    }
+  }
+
+  return animalCounts;
+}
+
+async function createGuessForGame(gameID,guess,bullsCount,cowsCount){
+  return await Guess.query().insert({
+    guess:guess,
+    gameId:gameID,
+    bullsCount:bullsCount,
+    cowsCount:cowsCount
+    })
+}
 
 async function getAllGuessesByGameId(gameID){
     return await Guess.query().where("gameId",gameID);
