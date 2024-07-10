83de749fa38caa80107370a05a9977c28792d4d8
Martin-Andonov <85078216+Martin-Andonov@users.noreply.github.com>
2024-07-10 13:41:15 +0300

    added front end game page and functionality with fetch

---
+++ client/game/script.js
@@ -0,0 +1,108 @@
+
+let numberOfGuesses = 0;
+let baseUrl = "http://localhost:3000";
+
+document.getElementById("submit").addEventListener("click", async function(event){
+    event.preventDefault();
+    const number = document.getElementById("guessInput").value;
+   
+
+    if (!/^(?!.*(.).*\1)\d{4}$/.test(number)) 
+    {
+        console.log("Error in validation!");
+        createElementError("Enter number consisting of 4 unique digits!");
+    }else{
+        numberOfGuesses++;
+
+        let serverResponse = await getData(number);
+        console.log(serverResponse); 
+
+        if(serverResponse["status"] == "fail")
+        {
+            createElementError(serverResponse["message"])
+        }else {
+            if(serverResponse["bullsCount"] == 4 || serverResponse["hasGuessed"] == true )
+            {
+                console.log("win");
+                //create saveGame
+                createElementGuess(number,serverResponse["cowsCount"],serverResponse["bullsCount"],serverResponse["hasGuessed"])
+            }else{
+                createElementGuess(number,serverResponse["cowsCount"],serverResponse["bullsCount"],serverResponse["hasGuessed"]);
+            }
+        }
+        
+    }
+});
+
+
+async function getData(guess)
+{
+    try {
+        const request = new Request(generataFullUrl("/guess/create/1"), {
+            method: "POST",
+            headers: {'Content-Type': 'application/json',},
+            body: JSON.stringify({ "guess": guess }),
+        });
+        
+        const response = await fetch(request);
+        
+        if (!response.ok) {
+          throw new Error(`Response status: ${response.status}`); 
+        }
+    
+        const json = await response.json();
+        
+        return json;
+
+      } catch (error) {
+        console.error(error.message); // change this to display error in element
+      }
+}
+
+
+
+function createElementError(string)
+{
+    const newLi = document.createElement('li');
+    const errorDisplayDiv = document.createElement('div');
+    errorDisplayDiv.className = 'error-display';
+    errorDisplayDiv.textContent = string;
+    newLi.append(errorDisplayDiv);
+    document.querySelector('.guess-list').appendChild(newLi);
+}
+
+function createElementGuess(guess,bulls,cows,hasGuessed)
+{
+    const newLi = document.createElement('li');
+    if(hasGuessed)  newLi.classList.add('green');
+
+    const guessDisplayDiv = document.createElement('div');
+    guessDisplayDiv.className = 'guess-display';
+    guessDisplayDiv.textContent = guess; 
+    
+
+    const numberOfAnimalsContainerDiv = document.createElement('div');
+    numberOfAnimalsContainerDiv.className = 'number-of-animals-container';
+    
+    const bullsCountDiv = document.createElement('div');
+    bullsCountDiv.className = 'bulls-count';
+    bullsCountDiv.textContent = bulls; 
+    
+    const cowsCountDiv = document.createElement('div');
+    cowsCountDiv.className = 'cows-count';
+    cowsCountDiv.textContent = cows;
+    
+    numberOfAnimalsContainerDiv.appendChild(bullsCountDiv);
+    numberOfAnimalsContainerDiv.appendChild(cowsCountDiv);
+    
+    newLi.appendChild(guessDisplayDiv);
+    newLi.appendChild(numberOfAnimalsContainerDiv);
+    
+    document.querySelector('.guess-list').appendChild(newLi);
+}
+
+
+function generataFullUrl(path)
+{
+    return baseUrl + path;
+}
