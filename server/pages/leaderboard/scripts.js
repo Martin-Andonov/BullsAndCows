24690293805884cd1b5c3822f7efe89d7c5b3006
Martin-Andonov <85078216+Martin-Andonov@users.noreply.github.com>
2024-07-11 13:49:05 +0300

    added navbar to leaderboards

--- server/pages/leaderboard/scripts.js
+++ server/pages/leaderboard/scripts.js
@@ -1,4 +1,10 @@
 document.getElementById('myButton').addEventListener('click', function() {
     document.getElementById('message').textContent = 'Hello, World!';
   });
+
+document.getElementById("new-game-navbar").addEventListener("click",async function(event){
+  event.preventDefault();
+  startNewGame();
+});
+
   
